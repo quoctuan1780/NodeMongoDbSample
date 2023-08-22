8172dcb933965c929b3614ea396fecd9eea05df2
Quoc Tuan <Quoctuan1789@outlook.com>
2023-08-22 22:27:27 +0700

    Updated

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
     port: 8000,
-    dbUri: "mongodb://localhost:27017/batterythuduc"
+    dbUri: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yzqzpsx.mongodb.net/batterythuduc?retryWrites=true&w=majority`
 };
